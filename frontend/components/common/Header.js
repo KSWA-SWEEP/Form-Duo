d7b626967b82f387c38d77da809cc5f302e5df21
SUPERKEON\keon <up2sky7@naver.com>
2022-10-12 16:11:45 +0900

    Kakao Login Added.

--- frontend/components/common/Header.js
+++ frontend/components/common/Header.js
@@ -4,6 +4,7 @@ import Image from 'next/future/image';
 import logoMixed from '../../public/img/mixed@3x.png'
 import logoIcon from '../../public/img/icon@3x.png'
 import Link from 'next/link';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 import { Bars3Icon, DocumentTextIcon, MicrophoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
@@ -48,6 +49,192 @@ function classNames(...classes) {
 }
 
 export default function Header() {
+  const { data: session} = useSession()
+  console.log(session);
+  if(session){
+    return (
+      <>
+        <div className="min-h-full mt-5">
+          <Disclosure as="nav">
+            {({ open }) => (
+              <>
+                <div className="px-4 mx-auto border-b-2 border-gray-100 max-w-7xl sm:px-6 lg:px-8">
+                  <div className="flex items-center justify-between h-24">
+  
+                    {/* logo */}
+                    <div className="flex items-center duration-200 hover:scale-105">
+                      <div className="flex-shrink-0">
+                      <Link 
+                          href={{
+                              pathname: '/'
+                          }} 
+                      > 
+                        <div className="flex items-center">
+                          <Image
+                            className="w-auto h-12 mr-3"
+                            src={logoIcon}
+                            alt="Form Duo"
+                          />
+                          <Image
+                            className="w-auto h-10"
+                            src={logoMixed}
+                            alt="Form Duo"
+                          />
+                        </div>
+                      </Link>
+                      </div>
+                    </div>
+                    
+                    {/* menu */}
+                    <div className="flex items-center">
+                      <div className="hidden md:block">
+                        <div className="flex items-baseline space-x-6">
+                          {navigation.map((item) => (
+                            (
+                              item.name == "설문 제작"
+                              ?
+                              // 설문 제작의 경우 바로 페이지로 넘어가지 않고 일반/듀오 선택
+                              <Menu as="div" className="relative ml-3" key={item.name}>
+                                    <div>
+                                        <Menu.Button>
+                                          <a
+                                            className={classNames(
+                                              item.current
+                                                ? 'text-fdblue'
+                                                : 'text-gray-700 hover:bg-fdbluelight hover:opacity-70 hover:text-white',
+                                              'px-3 py-2 rounded-lg text-base font-semibold hover:scale-105 duration-200'
+                                            )}
+                                            aria-current={item.current ? 'page' : undefined}
+                                          >
+                                            {item.name}
+                                          </a>
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                    as={Fragment}
+                                    enter="transition ease-out duration-100"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95"
+                                    >
+                                    <Menu.Items className="absolute z-10 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        {svyType.map((item) => (
+                                            <Menu.Item key={item.name}>
+                                                  {({ active }) => (
+                                                  <a
+                                                      href={item.href}
+                                                      className={classNames(
+                                                      active ? 'bg-neutral-100' : '',
+                                                      'content-center block px-4 py-2 text-sm font-bold text-gray-700 border-b-2 border-gray-100'
+                                                      )}
+                                                  >
+                                                      <div className='flex items-center'>
+                                                        {/* 타입에 따라 icon 변경 */}
+                                                        {
+                                                            {
+                                                              'DocumentTextIcon': <DocumentTextIcon className='w-4 h-4 mr-2'/>,
+                                                              'MicrophoneIcon': <MicrophoneIcon className='w-4 h-4 mr-2'/>,
+                                                            }[item.icon]
+                                                        }
+                                                        {item.name}
+                                                      </div>
+                                                  </a>
+                                                )}
+                                            </Menu.Item>
+                                        ))}
+                                    </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                              :
+                                <Link
+                                  key={item.name}
+                                  href={{
+                                      pathname: item.href
+                                    }} 
+                                  className={classNames(
+                                    item.current
+                                      ? 'text-fdblue'
+                                      : 'text-gray-700 hover:bg-fdbluelight hover:opacity-70 hover:text-white',
+                                    'px-3 py-2 rounded-lg text-base font-semibold hover:scale-105 duration-200'
+                                  )}
+                                  aria-current={item.current ? 'page' : undefined}
+                                  >
+                                  {item.name}
+                                </Link>
+                              )
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+  
+                    <>
+                        Hello, {session.user.name} <br/>
+                        <button onClick={() => signOut()}>Sign out</button>
+                    </>
+                    
+                    <div className="flex -mr-2 md:hidden">
+                      {/* Mobile menu button */}
+                      <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-fdblue hover:bg-fdbluelight hover:text-fdblue focus:outline-none focus:ring-2 focus:ring-white">
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
+                        ) : (
+                          <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
+                        )}
+                      </Disclosure.Button>
+                    </div>
+                  </div>
+                </div>
+  
+                <Disclosure.Panel className="md:hidden">
+                  <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    {navigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-fdblue text-white' : 'text-gray-500 hover:bg-fdblue hover:text-white',
+                          'block px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                  <div className="pt-5 pb-3 border-t border-gray-500 border-opacity-40">
+                    <div className="flex items-center px-5">
+                      <div className="flex-shrink-0">
+                        <img className="w-10 h-10 rounded-full" src={user.imageUrl} alt="" />
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-sm font-medium leading-none text-gray-500">{user.email}</div>
+                      </div>
+                    </div>
+                    <div className="px-2 mt-3 space-y-1">
+                      {userNavigation.map((item) => (
+                        <Disclosure.Button
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block px-3 py-2 text-sm font-medium text-gray-500 rounded-md hover:bg-neutral-500 hover:text-white"
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      ))}
+                    </div>
+                  </div>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </div>
+      </>
+    )
+  }
   return (
     <>
       <div className="min-h-full mt-5">
@@ -164,76 +351,19 @@ export default function Header() {
                       </div>
                     </div>
                   </div>
-
-                  {/* account */}
-
-                  {/* account (before login) */}
-                  {/* <div className="hidden md:block">
+                  <div className="hidden md:block">
                     <div className="flex items-center ml-4 md:ml-6">
                       <div className="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
-                        <Link
-                          href={{
-                              pathname: "/auth/signIn"
-                          }} 
-                        >
-                          <div className="text-sm font-normal text-gray-500 duration-200 whitespace-nowrap hover:text-fdbluedark hover:scale-105">
-                            Sign In
-                          </div>
-                        </Link>
-                        <Link
-                          href={{
-                              pathname: "/auth/signUp"
-                          }} 
-                        >
+                          <button onClick={() => signIn("kakao")} className="text-sm font-normal text-gray-500 duration-200 whitespace-nowrap hover:text-fdbluedark hover:scale-105">
+                            Sign In with Kakao
+                          </button>
                           <div className="inline-flex items-center justify-center px-3 py-2 ml-8 text-sm font-normal text-white duration-200 border border-transparent rounded-md shadow-sm whitespace-nowrap bg-fdblue hover:bg-fdbluedark hover:scale-105">
                             Sign Up
                           </div>
-                        </Link>
                       </div>
                     </div>
-                  </div> */}
-                  
-                  {/* account (after login) */}
-                  <div className="hidden md:block">
-                    <div className="flex items-center ml-4 md:ml-6">
-                      <div className="text-sm font-normal leading-none text-gray-400">{user.email}</div>
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="flex items-center max-w-xs text-sm rounded-full bg-neutral-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2">
-                            <span className="sr-only">Open user menu</span>
-                            <img className="w-8 h-8 rounded-full" src={user.imageUrl} alt="" />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg w-28 ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-neutral-100' : '',
-                                      'block px-4 py-2 text-xs font-semibold '
-                                    ) + ( item.name == "로그아웃" ? "text-red-600" : "text-gray-700")}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
-                    </div>
                   </div>
+
                   
                   <div className="flex -mr-2 md:hidden">
                     {/* Mobile menu button */}
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 import * as Sentry from "@sentry/nextjs"
 import { Integrations } from '@sentry/nextjs'
 import axios from 'axios';
-
+import { SessionProvider } from "next-auth/react"
 
 // console.log(process.env.NEXT_PUBLIC_API_URL);
 // const apiUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -12,10 +12,15 @@ axios.defaults.baseURL = 'http://210.109.60.247:10001';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Layout>
-      <Component {...pageProps}/>
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
