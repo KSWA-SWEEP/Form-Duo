eb10244043d353f1e94d454362b4bb3d4546393f
Jangkeon <up2sky7@naver.com>
2022-10-13 13:53:22 +0900

    Ready for Survey Modify

--- frontend/components/common/Header.js
+++ frontend/components/common/Header.js
@@ -52,18 +52,19 @@ export default function Header() {
   const { data: session} = useSession()
   console.log(session);
   if(session){
+    // 로그인 됬을 때
     return (
       <>
         <div className="min-h-full mt-5">
           <Disclosure as="nav">
             {({ open }) => (
               <>
-                <div className="px-4 mx-auto border-b-2 border-gray-100 max-w-7xl sm:px-6 lg:px-8">
+                <div className="px-4 mx-auto border-b-2 border-gray-200 max-w-7xl sm:px-6 lg:px-5">
                   <div className="flex items-center justify-between h-24">
   
                     {/* logo */}
-                    <div className="flex items-center duration-200 hover:scale-105">
-                      <div className="flex-shrink-0">
+                    <div className="flex items-center duration-50 hover:scale-105">
+                      <div className="flex-grow-0">
                       <Link 
                           href={{
                               pathname: '/'
@@ -170,7 +171,7 @@ export default function Header() {
                     </div>
   
                     <>
-                        Hello, {session.user.name} <br/>
+                        반갑습니다, {session.user.name}님  <br/>
                         <button onClick={() => signOut()}>Sign out</button>
                     </>
                     
