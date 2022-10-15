c93875dbe8712027e6184caa158dd203c9583dde
banggeunho <panggeunho@gmail.com>
2022-10-16 04:07:47 +0900

    [FE] "설문 응답 CSV 저장 모듈 추가" [BE] "유저네임 조회 기능 추가"

--- backend/src/main/java/com/sweep/formduo/domain/members/MemberRepository.java
+++ backend/src/main/java/com/sweep/formduo/domain/members/MemberRepository.java
@@ -2,6 +2,7 @@ package com.sweep.formduo.domain.members;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Members,Long> {
