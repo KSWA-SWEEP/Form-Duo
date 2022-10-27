6b09dd37b5b5725ac63c8daa22169e61fc8127db
Jangkeon <up2sky7@naver.com>
2022-10-27 10:57:22 +0900

    [SWEEP-98] Build Error Handle

--- frontend/pages/survey/emotion/[convId].js
+++ frontend/pages/survey/emotion/[convId].js
@@ -1,15 +1,15 @@
 import { useRouter } from 'next/router'
 import { useRecoilState } from 'recoil';
-import Conversation from '../../../components/ui/survey/emotion/conversation';
+import Conversation from '../../../components/ui/survey/emotion/Conversation';
 import PageTitle from '../../../components/ui/PageTitle';
 
 export default function ConversationAnalysis() {
     const router = useRouter();
-    const { convid } = router.query;
+    const { cvid } = router.query;
 
     return (
         <>
-            <Conversation cvid = {convid}/>
+            <Conversation cvid = {cvid}/>
         </>
     );
 };
