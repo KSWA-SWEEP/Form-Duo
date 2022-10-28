968ec93835c31f8acb860fb2cb1f0f0eec1784b9
ssoyeong <jsy171771@gmail.com>
2022-10-29 03:37:57 +0900

    [FE] 듀오 설문 미리보기 이슈 해결

--- frontend/components/ui/survey/SurveyPreview.js
+++ frontend/components/ui/survey/SurveyPreview.js
@@ -10,9 +10,10 @@ export default function SurveyPreview(props) {
     const [svyRespContents, setSvyRespContents] = useState(null);
     const [isLoading, setLoading] = useState(false);
     const [preURL, setPreURL] = useState(props.preURL);
+    const [svyType, setSvyType] = useState(props.svyType);
     const router = useRouter();
 
-    useEffect( () => {
+    useEffect(() => {
         if (!props.svyResContents) {
             initResContents();
             // initPreURL();
@@ -27,7 +28,7 @@ export default function SurveyPreview(props) {
         let resContent = [];
         let newList = [];
         svyContents.svyContent && svyContents.svyContent.map(question => {
-            resContent = {qId: question.qId, qType: question.qType, ansVal: [{qContentId: "", resp: ""}]}
+            resContent = { qId: question.qId, qType: question.qType, ansVal: [{ qContentId: "", resp: "" }] }
             newList = [...newList, resContent];
             setSvyRespContents(newList);
         })
@@ -41,17 +42,17 @@ export default function SurveyPreview(props) {
         <div>
             {/* 제목 입력 */}
             <SurveyTitleShow bgColor="bg-fdyellowbright"
-                             svyTitle={svyContents.svyTitle}
-                             svyIntro={svyContents.svyIntro}
+                svyTitle={svyContents.svyTitle}
+                svyIntro={svyContents.svyIntro}
             />
             {/* 질문 목록 */}
-            <ShowQuestionList svyContents={svyContents} svyRespContents={svyRespContents} setSvyRespContents={setSvyRespContents} isModify={props.isModify}/>
+            <ShowQuestionList svyContents={svyContents} svyRespContents={svyRespContents} setSvyRespContents={setSvyRespContents} isModify={props.isModify} />
 
             <div className="flex justify-center mx-2 rounded-md m-7 ">
-                <a onClick={ () => router.push({
-                    pathname : preURL
+                <a onClick={() => router.push({
+                    pathname: preURL
                 })}
-                   className="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-white bg-blue-400 border border-transparent rounded-md hover:bg-blue-500"
+                    className="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-white bg-blue-400 border border-transparent rounded-md hover:bg-blue-500"
                 >
                     뒤로가기
                 </a>
