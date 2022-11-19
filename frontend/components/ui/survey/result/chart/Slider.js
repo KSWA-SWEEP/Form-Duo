5be6827c1b8eb9f2353efacf6413bedd0120c50d
ssoyeong <jsy171771@gmail.com>
2022-11-19 23:37:05 +0900

    [SWEEP-103] 설문 분석 UI 수정

--- frontend/components/ui/survey/result/chart/Slider.js
+++ frontend/components/ui/survey/result/chart/Slider.js
@@ -3,14 +3,14 @@ import { styled } from '@mui/material/styles';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 10,
-    borderRadius: 5,
+    height: 30,
+    borderRadius: 15,
     [`&.${linearProgressClasses.colorPrimary}`]: {
         backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
     },
     [`& .${linearProgressClasses.bar}`]: {
         borderRadius: 5,
-        backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+        backgroundColor: theme.palette.mode === 'light' ? '#7892b0' : '#308fe8',
     },
 }));
 
--- frontend/pages/survey/result/[surveyId].js
+++ frontend/pages/survey/result/[surveyId].js
@@ -6,9 +6,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Slider } from "../../../components/ui/survey/result/chart/Slider";
 import CheckAxiosToken from "../../../components/customAxios/checkAccessToken";
-import {accToken} from "../../../atoms/accToken";
-import {useRecoilState} from "recoil";
+import { accToken } from "../../../atoms/accToken";
+import { useRecoilState } from "recoil";
 import CustomAxios from "../../../components/customAxios/customAxios";
+import { Tab } from '@headlessui/react'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 const SurveyResult = () => {
 
@@ -22,9 +27,14 @@ const SurveyResult = () => {
     const [isLoading, setLoading] = useState(false)
     const [viewChart, setViewChart] = useState(true);
     //Access Token
-    const [acctoken,setAcctoken] = useRecoilState(accToken);
+    const [acctoken, setAcctoken] = useRecoilState(accToken);
 
     // if (!isLoading) getContents(Object.values(router.query))
+
+    let [categories] = useState({
+        "설문 데이터 분석": [],
+        "개별 응답 조회": [],
+    })
 
 
     useEffect(() => {
@@ -39,7 +49,7 @@ const SurveyResult = () => {
     // alert(message + " " + surveyId);
 
     useEffect(() => {
-        if (surveyId) getContents(surveyId).then(r => {});
+        if (surveyId) getContents(surveyId).then(r => { });
     }, [surveyId])
 
 
@@ -52,9 +62,9 @@ const SurveyResult = () => {
             // setLoading(false);
             // return svyContents;
 
-            CheckAxiosToken(acctoken).then(r=>{
+            CheckAxiosToken(acctoken).then(r => {
                 setAcctoken(r)
-                CustomAxios('get','/api/v1/surveys/' + surveyId + '/resp', r, {}).then(r=>{
+                CustomAxios('get', '/api/v1/surveys/' + surveyId + '/resp', r, {}).then(r => {
                     setData(r.data);
                 })
             })
@@ -80,69 +90,89 @@ const SurveyResult = () => {
     else {
         if (surveyType === "duo") return (
             <div>
-                <div>
-                    <div className="py-1 mt-1 overflow-auto rounded-md shadow-lg max-h-30 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-
-                        <h1 className='dark:text-neutral-200'>설문 참여율 : {data.length} / {data[0].svyRespsMax}</h1>
-                        <Slider data={parseInt((data.length / data[0].svyRespsMax * 100).toString())} />
-
+                <div className="px-10 mt-1 overflow-auto rounded-md max-h-30 focus:outline-none ">
+                    <div className='flex justify-between'>
+                        <div className='font-semibold'>설문 참여율</div>
+                        <div className='font-semibold'>{data.length} / {data[0].svyRespsMax}</div>
                     </div>
-                    <br />
-                    <SurveyResults svyType={surveyType} resPeople={data.length} maxResPeople={data[0].svyRespsMax} resContents={Object.values(data)}
-                    />
-
+                    <Slider data={parseInt((data.length / data[0].svyRespsMax * 100).toString())} />
+                </div>
+                <div className="max-w-2xl px-4 mx-60 py-8 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <div className="mt-5 mb-7">
+                        <Tab.Group>
+                            <Tab.List className="flex p-1 space-x-1 bg-blue-900/5 rounded-xl">
+                                <Tab
+                                    key={"개별 응답 조회"}
+                                    className={({ selected }) =>
+                                        classNames(
+                                            'w-full rounded-lg py-2.5 text-sm font-bold leading-5',
+                                            'ring-white ring-opacity-60 ring-offset-2 focus:outline-none focus:ring-2',
+                                            selected
+                                                ? 'text-white bg-blue-900/40 dark:bg-fdyellowlight dark:text-neutral-600'
+                                                : 'text-neutral-500 hover:bg-blue-900/20 hover:text-fdbluedark dark:bg-neutral-500/40 dark:hover:bg-amber-300/80 dark:hover:text-neutral-100 dark:text-neutral-200'
+                                        )
+                                    }
+                                >
+                                    개별 응답 조회
+                                </Tab>
+                            </Tab.List>
+                            <Tab.Panels>
+                                <Tab.Panel>
+                                    <br />
+                                    <SurveyResults surveyType={surveyType} resPeople={data.length} maxResPeople={data[0].svyRespsMax} resContents={Object.values(data)} />
+                                </Tab.Panel>
+                            </Tab.Panels>
+                        </Tab.Group>
+                    </div>
                 </div>
             </div>
         )
 
         return (
             <div>
-                <div
-                >
-                    <div className="py-1 mt-1 overflow-auto rounded-md shadow-lg max-h-30 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-
-                        <h1>설문 참여율 : {data.length} / {data[0].svyRespsMax}</h1>
-                        <Slider data={parseInt((data.length / data[0].svyRespsMax * 100).toString())} />
-
+                <div className="px-10 mt-1 overflow-auto rounded-md max-h-30 focus:outline-none ">
+                    <div className='flex justify-between'>
+                        <div className='font-semibold'>설문 참여율</div>
+                        <div className='font-semibold'>{data.length} / {data[0].svyRespsMax}</div>
                     </div>
-
-                    <br />
-                    <div align="center">
-                        {viewChart ? (
-                            <>
-                                <button onClick={() => setViewChart(true)}
-                                    className="inline-flex items-center justify-center px-3 py-2 ml-8 text-sm font-normal text-white duration-200 border border-transparent rounded-md shadow-sm whitespace-nowrap bg-fdblue hover:bg-fdbluedark hover:scale-105">
-                                    개별 응답 조회
-                                </button>
-                                <button onClick={() => setViewChart(false)}
-                                    className="inline-flex items-center justify-center px-3 py-2 ml-8 text-sm font-normal text-white duration-200 border border-transparent rounded-md shadow-sm whitespace-nowrap bg-fdblue hover:bg-fdbluedark hover:scale-105">
-                                    설문 데이터 분석
-                                </button>
-                            </>
-                        ) : (
-                            <>
-                                <button onClick={() => setViewChart(true)}
-                                    className="inline-flex items-center justify-center px-3 py-2 ml-8 text-sm font-normal text-white duration-200 border border-transparent rounded-md shadow-sm whitespace-nowrap bg-fdblue hover:bg-fdbluedark hover:scale-105">
-                                    개별 응답 조회
-                                </button>
-                                <button onClick={() => setViewChart(false)}
-                                    className="inline-flex items-center justify-center px-3 py-2 ml-8 text-sm font-normal text-white duration-200 border border-transparent rounded-md shadow-sm whitespace-nowrap bg-fdblue hover:bg-fdbluedark hover:scale-105">
-                                    설문 데이터 분석
-                                </button>
-                            </>
-                        )}
-                    </div>
+                    <Slider data={parseInt((data.length / data[0].svyRespsMax * 100).toString())} />
                 </div>
-                <div>
-                    {viewChart ? <SurveyResults resPeople={data.length} maxResPeople={data[0].svyRespsMax} resContents={Object.values(data)}
-                    />
-                        :
-                        <SurveyAnalysis resPeople={data.length} maxResPeople={data[0].svyRespsMax} resContents={Object.values(data)} />}
+                <div className="max-w-2xl px-4 py-8 mx-auto sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <div className="mt-5 mb-7">
+                        <Tab.Group>
+                            <Tab.List className="flex p-1 space-x-1 bg-blue-900/5 rounded-xl">
+                                {Object.keys(categories).map((category) => (
+                                    <Tab
+                                        key={category}
+                                        className={({ selected }) =>
+                                            classNames(
+                                                'w-full rounded-lg py-2.5 text-sm font-bold leading-5',
+                                                'ring-white ring-opacity-60 ring-offset-2 focus:outline-none focus:ring-2',
+                                                selected
+                                                    ? 'text-white bg-blue-900/40 dark:bg-fdyellowlight dark:text-neutral-600'
+                                                    : 'text-neutral-500 hover:bg-blue-900/20 hover:text-fdbluedark dark:bg-neutral-500/40 dark:hover:bg-amber-300/80 dark:hover:text-neutral-100 dark:text-neutral-200'
+                                            )
+                                        }
+                                    >
+                                        {category}
+                                    </Tab>
+                                ))}
+                            </Tab.List>
+                            <Tab.Panels>
+                                <Tab.Panel>
+                                    <SurveyAnalysis resPeople={data.length} maxResPeople={data[0].svyRespsMax} resContents={Object.values(data)} />
+                                </Tab.Panel>
+                                <Tab.Panel>
+                                    <br />
+                                    <SurveyResults surveyType={surveyType} resPeople={data.length} maxResPeople={data[0].svyRespsMax} resContents={Object.values(data)} />
+                                </Tab.Panel>
+                            </Tab.Panels>
+                        </Tab.Group>
+                    </div>
                 </div>
             </div>
         );
     }
-    ;
 }
 
 export default SurveyResult;
